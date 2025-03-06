781dfc291ea61a6e2277563905648501ca07ced8
gamerjuniorjr07@gmail.com <gamerjuniorjr07@gmail.com>
2025-03-06 11:24:06 -0300

    Lisboa 1.0

---
+++ siteviagem/src/pages/Lisboa.jsx
@@ -0,0 +1,31 @@
+import FotoLisba from '../assets/Lisboa.jpg'
+
+function Lisboa() {
+    return (
+        <div>
+
+<img src={FotoLisba}/>
+
+            <h1>Lisboa🌆</h1>
+
+            <p>Lisboa, a capital de Portugal, é uma cidade vibrante e cheia de história, situada às margens do rio Tejo.
+                Conhecida por suas colinas, ruas de paralelepípedos e arquitetura encantadora, Lisboa combina o antigo e o moderno de maneira única.
+                Os visitantes podem explorar bairros icônicos como Alfama, com suas ruelas estreitas e casas coloridas, e Belém, famoso por seus monumentos históricos e os deliciosos pastéis de nata.
+                A cidade também é famosa por sua vida noturna animada, deliciosa gastronomia e uma rica cena cultural, incluindo museus, teatros e festivais.
+                Com um clima ameno e vistas deslumbrantes, Lisboa é um destino que encanta a todos que a visitam.</p>
+
+            <h2>O que fazer 3 dias em Lisboa?</h2>
+
+            <p>Dia 1
+                Pode ir ao bairro Alfama, onde irá passar por ruas estreitas e visitar a Catedral de Lisboa, Logo em seguida vá para o Castelo de São Jorge para visitar as muralhas e ver uma bela vista.
+
+                Dia 2
+                Visite a Torre de Bélem ou o Mosteiro dos Jerónimos, e não deixe de experimentar os pasteis de belém e de nata, Mais tarde explore o Museu Calouste Gulbenkian, pela noite pode ir para o bairro alto conecido pela sua famosa vibe noturna.
+
+                Dia 3
+                Comece o dia visitando o Parque das Nações, onde ira conseguir explorar o aquario de Lisboa, mais tarde visite o Pavilhão do Conhecimento, no fim da viajem para relaxar jante em algum restaurante com vista para o rio.</p>
+        </div>
+    )
+}
+
+export default Lisboa
