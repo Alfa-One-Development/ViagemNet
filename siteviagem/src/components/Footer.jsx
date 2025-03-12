45818d11009b665fb3c506be9e7af6afe6dbfb53
Felipe Cagnin <cagnin.lima.2007@gmail.com>
2025-03-12 14:47:28 -0300

    🎈

--- siteviagem/src/components/Footer.jsx
+++ siteviagem/src/components/Footer.jsx
@@ -5,6 +5,7 @@ function Footer() {
             <footer>
                 <h4>AlphaOne Development</h4>
                 <h6>Transformando ideias em soluções inovadoras para um futuro tecnológico.</h6>
+                <h6>Membros: Felipe C, Murilo B, Murilo C, Vitor H e William Jr</h6>
                 <p>Copyright 2025 - Todos os direitos reservados</p>
             </footer>
         </>
