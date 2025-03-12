3dd2e993a2c414f60a91c77c78b4d2dc414ffa76
Felipe Cagnin <cagnin.lima.2007@gmail.com>
2025-03-12 10:49:23 -0300

    VUltimate3000

--- siteviagem/src/components/Footer.jsx
+++ siteviagem/src/components/Footer.jsx
@@ -2,9 +2,11 @@ import "../styles/Footer.css"
 function Footer() {
     return (
         <>
-        <footer>
-            <h4>AlphaOne Development</h4>
-        </footer>
+            <footer>
+                <h4>AlphaOne Development</h4>
+                <h6>Transformando ideias em soluções inovadoras para um futuro tecnológico.</h6>
+                <p>Copyright 2020 - Todos os direitos reservados</p>
+            </footer>
         </>
     )
 }
